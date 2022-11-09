5db0862bdaf8decbbeafbffae74c201b52e2e4a4
Leonardo Picchi Dancona <lpdancona@gmail.com>
2022-11-08 16:53:42 -0800

    packing list using while loop

--- packing-list.js
+++ packing-list.js
@@ -8,8 +8,14 @@ const packingList = [
   "cups",
   "cooking utensils",
 ];
-
+// for loop
+// console.log("Kitchen stuff to pack:");
+// for (let i = 0; i < packingList.length; i++) {
+//   console.log(packingList[i]);
+// }
 console.log("Kitchen stuff to pack:");
-for (let i = 0; i < packingList.length; i++) {
+let i = 0;
+while (i < packingList.length) {
   console.log(packingList[i]);
+  i++;
 }
